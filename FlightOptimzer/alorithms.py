1b66c8a299e4ca8c46e79fc2127a509dd9a0a5e7
Westopoli <westleyyarlott@gmail.com>
2026-04-23 11:24:57 -0400

    bfs finished

--- FlightOptimzer/alorithms.py
+++ FlightOptimzer/alorithms.py
@@ -4,12 +4,27 @@
 # Called by: main.py (for demos), experiments.py (for timing benchmarks)
 
 def bfs(graph, start):
-    visited = set()     # tracks visited nodes
-    queue = [start]     # list of next nodes
-    parent = {}         # will track how we got to each node
-    depth = 1
+    visited = {start}         # tracks visited nodes
+    queue = [start]         # list of next nodes
+    distances = {start: 0}  # depth tracking
 
     while queue:
-        node, depth = queue.pop(0)  # unpack first tuple
-
+        # This logic was the first try, ended up being flawed
+        # node, node_depth = queue.pop(0)  # unpack first tuple
+        # while node_depth > current_depth:
+        #     current_depth += 1
         
+        # if node not in path:
+        #     visited.add(node)
+        #     path.append(node)
+
+        current = queue.pop(0)
+        # _ for distance, we don't need it for BFS
+        for  (location, _) in graph.locations(current):
+            if location not in visited:
+                visited.add(location)
+                distances[location] = distances[current] + 1
+                queue.append(location)
+
+    return distances
+
